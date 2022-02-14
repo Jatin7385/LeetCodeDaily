3a1c84dc521b894a98f8313c02137ffce53cab62
Jatin7385 <jats7385@gmail.com>
2022-02-14 14:43:52 +0530

    Comment added

--- Maximum_Depth_Of_Binary_Tree.java
+++ Maximum_Depth_Of_Binary_Tree.java
@@ -17,6 +17,8 @@ class Solution {
     public int maxDepth(TreeNode root) 
     {
         if(root ==null) return 0;
+	// So we first go till the leaf node, using recursion. While returning, the first value that will be returned is 0.
+	// Then we take the max of the return values and add 1 to it(Incrementing the depth).
         return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
     }
 }
